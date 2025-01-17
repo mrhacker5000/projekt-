bf753ee92bc75317cdc0541892d50278aba04d08
MrFlyGuy69 <s245878@dtu.dk>
2025-01-17 11:10:30 +0100

    Add files via upload

---
+++ Inc/GPIO.h
@@ -0,0 +1,20 @@
+/*
+ * GPIO.h
+ *
+ *  Created on: 14. jan. 2025
+ *      Author: phill
+ */
+
+#ifndef GPIO_H_
+#define GPIO_H_
+
+// Includes :
+#include "ansi.h"
+#include "stdint.h"
+#include "stdio.h"
+#include "30010_io.h"
+
+int8_t readjoystick() ;
+void setled() ;
+
+#endif /* GPIO_H_ */
---
+++ Inc/ansi.h
@@ -0,0 +1,50 @@
+/*
+ * ansi.h
+ *
+ *  Created on: 7. jan. 2025
+ *      Author: phill
+ */
+
+#ifndef ANSI_H_
+#define ANSI_H_
+
+/* Includes --------------------------------------------*/
+#include "stdint.h"
+#include "stdio.h"
+/* Exported types --------------------------------------*/
+/* Exported constants ----------------------------------*/
+/* Exported macro --------------------------------------*/
+/* Exported functions ----------------------------------*/
+
+void fgcolor (uint8_t foreground) ;
+void bgcolor (uint8_t background) ;
+void color (uint8_t foreground, uint8_t background) ;
+void underline (uint8_t on) ;
+void gotoxy(uint8_t y, uint8_t x) ;
+void blink(uint8_t on) ;
+void inverse(uint8_t on) ;
+void clrscr() ;
+void clreol() ;
+void clear() ;
+void window(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, char *title, uint8_t style) ;
+void drawball(int8_t x, int8_t y) ;
+void graphic() ;
+
+#define start1 180
+#define start2 185
+#define end1 195
+#define end2 204
+#define vert1 179
+#define vert2 186
+#define hor1 196
+#define hor2 205
+#define nw1 218
+#define nw2 201
+#define ne1 191
+#define ne2 187
+#define sw1 192
+#define sw2 200
+#define se1 217
+#define se2 188
+
+#endif /* ANSI_H_ */
